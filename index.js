8f953787c72b54ac1bb9d4be196b8c47c81168bc
Nidhi Parthasarathy <nparthasarathy675@student.fuhsd.org>
2024-07-14 11:50:38 -0700

    commit

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const path = require('path');
+require('dotenv').config(); // Add this line to load the environment variables
+
 const app = express();
 
 // Static files
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
+app.get('/api-key', (req, res) => {
+  res.json({ apiKey: process.env.OPENAI_API_KEY });
+});
+
+
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 // Add other routes as necessary
@@ -15,7 +22,7 @@ app.get('/', (req, res) => {
 
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
 
 module.exports = app;
